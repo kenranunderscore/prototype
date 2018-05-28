b69d11634fd8522291182e034ee005ddb129dd5a
kenranunderscore <johb.maier@gmail.com>
2018-05-28 22:31:15 +0200

    Show a basic SDL window

---
+++ prototype/Core.cs
@@ -0,0 +1,63 @@
+﻿namespace prototype.GUI
+{
+    using System;
+    using static SDL2.SDL;
+
+    internal class Core
+    {
+        private IntPtr window_;
+        private IntPtr renderer_;
+
+        public bool Initialize()
+        {
+            SDL_SetHint(SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING, "1");
+            if (SDL_Init(SDL_INIT_VIDEO) < 0)
+            {
+                return false;
+            }
+
+            window_ = SDL_CreateWindow(
+                "prototype",
+                SDL_WINDOWPOS_UNDEFINED,
+                SDL_WINDOWPOS_UNDEFINED,
+                800,
+                600,
+                SDL_WindowFlags.SDL_WINDOW_SHOWN);
+            if (window_ == IntPtr.Zero)
+            {
+                return false;
+            }
+
+            renderer_ = SDL_CreateRenderer(window_, 0, SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+
+            return true;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                if (SDL_PollEvent(out var evnt) != 0)
+                {
+                    if (evnt.type == SDL_EventType.SDL_QUIT
+                        || evnt.type == SDL_EventType.SDL_KEYDOWN
+                        && evnt.key.keysym.sym == SDL_Keycode.SDLK_ESCAPE)
+                    {
+                        break;
+                    }
+                }
+
+                SDL_SetRenderDrawColor(renderer_, 0xff, 0xff, 0xff, 0xff);
+                SDL_RenderClear(renderer_);
+                SDL_RenderPresent(renderer_);
+            }
+        }
+
+        public void Cleanup()
+        {
+            SDL_DestroyRenderer(renderer_);
+            SDL_DestroyWindow(window_);
+            SDL_Quit();
+        }
+    }
+}
--- prototype/Program.cs
+++ prototype/Program.cs
@@ -1,9 +1,20 @@
 ﻿namespace prototype.GUI
 {
+    using System;
+
     public class Program
     {
         public static void Main(string[] args)
         {
+            var core = new Core();
+            if (!core.Initialize())
+            {
+                Console.WriteLine("Core SDL functionality failed to initialize");
+                Environment.Exit(-1);
+            }
+
+            core.Run();
+            core.Cleanup();
         }
     }
 }
