5ab909bafd63305d7398aefd7dfc13f00b93a9b2
kenranunderscore <johb.maier@gmail.com>
2018-06-01 11:45:29 +0200

    Extract texture loading into separate class

---
+++ prototype/TextureLoader.cs
@@ -0,0 +1,19 @@
+﻿namespace prototype.GUI
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using static SDL2.SDL;
+    using static SDL2.SDL_image;
+
+    internal class TextureLoader
+    {
+        public TextureWrapper LoadTexture(string path, IntPtr renderer)
+        {
+            var surface = IMG_Load(path);
+            var typedSurface = Marshal.PtrToStructure<SDL_Surface>(surface);
+            var texture = SDL_CreateTextureFromSurface(renderer, surface);
+            SDL_FreeSurface(surface);
+            return new TextureWrapper(texture, renderer, typedSurface.w, typedSurface.h);
+        }
+    }
+}
--- prototype/TextureWrapper.cs
+++ prototype/TextureWrapper.cs
@@ -1,9 +1,7 @@
 ﻿namespace prototype.GUI
 {
     using System;
-    using System.Runtime.InteropServices;
     using static SDL2.SDL;
-    using static SDL2.SDL_image;
 
     internal class TextureWrapper
     {
@@ -12,21 +10,12 @@
         private readonly int width_;
         private readonly int height_;
 
-        private TextureWrapper(IntPtr texture, IntPtr renderer, int width, int height)
+        public TextureWrapper(IntPtr texture, IntPtr renderer, int width, int height)
         {
             texture_ = texture;
             renderer_ = renderer;
             width_ = width;
             height_ = height;
-        }
-
-        public static TextureWrapper FromFile(string path, IntPtr renderer)
-        {
-            var surface = IMG_Load(path);
-            var typedSurface = Marshal.PtrToStructure<SDL_Surface>(surface);
-            var texture = SDL_CreateTextureFromSurface(renderer, surface);
-            SDL_FreeSurface(surface);
-            return new TextureWrapper(texture, renderer, typedSurface.w, typedSurface.h);
         }
 
         public void Free()
