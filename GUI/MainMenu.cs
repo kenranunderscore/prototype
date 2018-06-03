883077fd38189ce9e0299eed1693e96299b0f82f
kenranunderscore <johb.maier@gmail.com>
2018-06-03 15:11:35 +0200

    Center the main menu vertically

--- GUI/MainMenu.cs
+++ GUI/MainMenu.cs
@@ -6,23 +6,38 @@
 
     internal class MainMenu
     {
-        private readonly IReadOnlyList<string> menuItemCaptions_ = new List<string>
+        private IReadOnlyList<MenuItem> menuItems_;
+        private readonly IReadOnlyCollection<string> menuItemCaptions_ = new List<string>
         {
             "Play",
             "Options",
             "Quit"
         };
-        private readonly IReadOnlyList<MenuItem> menuItems_;
 
         public MainMenu(TextRenderer textRenderer, Options options)
         {
-            var maxMenuItemWidth = menuItemCaptions_.Max(c => c.Length) * 8; //TODO get letter width here
-            var x = options.ScreenWidth / 2 - maxMenuItemWidth / 2; //TODO get letter height here and calculate y correctly
-            var y = options.ScreenHeight / 2;
+            CreateMenuItems(textRenderer, options);
+        }
+
+        private void CreateMenuItems(TextRenderer textRenderer, Options options)
+        {
+            //TODO refactor
+            var maxMenuItemWidth = menuItemCaptions_.Max(c => c.Length) * Defaults.LetterWidth;
+            var x = options.ScreenWidth / 2 - maxMenuItemWidth / Defaults.LetterWidth;
+            var centerY = options.ScreenHeight / 2;
+            var menuItemHeight = Defaults.LetterHeight;
+            var totalMenuHeight = (2 * menuItemCaptions_.Count - 1) * menuItemHeight;
+            var y = centerY - totalMenuHeight / 2;
             menuItems_ = menuItemCaptions_.Select((caption, i) => new MenuItem(
-                textRenderer,
-                caption,
-                new SDL.SDL_Rect { x = x, y = y + i * 20 }))
+                    textRenderer,
+                    caption,
+                    new SDL.SDL_Rect
+                    {
+                        x = x,
+                        y = y + 2 * i * menuItemHeight,
+                        w = caption.Length * Defaults.LetterWidth,
+                        h = Defaults.LetterHeight
+                    }))
                 .ToList();
         }
 
