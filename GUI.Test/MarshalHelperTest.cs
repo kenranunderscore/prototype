e909a971964eaa47980130bea350535f6d444617
kenranunderscore <johb.maier@gmail.com>
2018-06-09 17:29:13 +0200

    Correct an unsafe test

--- GUI.Test/MarshalHelperTest.cs
+++ GUI.Test/MarshalHelperTest.cs
@@ -6,9 +6,18 @@
     public class MarshalHelperTest
     {
         [Test]
-        public unsafe void Test()
+        public unsafe void Utf8_string_is_read_correctly_from_byte_pointer()
         {
+            var s = new TestStruct();
+            s.foo[0] = (byte)'F';
+            s.foo[1] = (byte)'o';
+            var utf8 = MarshalHelper.GetUtf8String(s.foo);
+            Assert.That(utf8, Is.EqualTo("Fo"));
+        }
 
+        private unsafe struct TestStruct
+        {
+            public fixed byte foo[3];
         }
     }
 }
