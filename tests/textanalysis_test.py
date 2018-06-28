fd76bce9033c29933e4435ee5f2ce1282a973848
kenranunderscore <johb.maier@gmail.com>
2018-06-28 23:03:02 +0200

    More word count tests

--- tests/textanalysis_test.py
+++ tests/textanalysis_test.py
@@ -4,3 +4,13 @@ from src import textanalysis
 def test_single_word_count():
     count = textanalysis.count_words('foobar')
     assert count == 1
+
+
+def test_count_of_multiple_words():
+    count = textanalysis.count_words('these are some more words')
+    assert count == 5
+
+
+def test_word_count_with_special_chars():
+    count = textanalysis.count_words('foo.bar and more;stuff! baz')
+    assert count == 4
