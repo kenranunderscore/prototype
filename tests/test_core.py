c8a0966f5d7f69eaccf4f39fb114a17f546f2cb2
kenranunderscore <johb.maier@gmail.com>
2018-06-30 20:01:17 +0200

    Refactor some tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,15 +2,19 @@ import pytest
 from src import core
 
 
-def test_program_exists_when_sdl_fails_to_load(mocker):
-    mocker.patch('src.core.sdl2.SDL_Init', return_value=-1)
-    mocker.patch('src.core.sdl2.SDL_GetError')
+@pytest.fixture
+def sdl(mocker):
+    return mocker.patch('src.core.sdl2')
+
+
+def test_program_exits_when_sdl_fails_to_load(mocker, sdl):
+    sdl.SDL_Init.return_value = -1
     with pytest.raises(SystemExit):
         c = core.Core(None, None)
 
-def test_program_exists_when_img_fails_to_load(mocker):
-    mocker.patch('src.core.sdl2.SDL_Init', return_value=0)
-    mocker.patch('src.core.sdl2.sdlimage.IMG_Init', return_value=-1)
-    mocker.patch('src.core.sdl2.SDL_GetError')
+
+def test_program_exits_when_img_fails_to_load(mocker, sdl):
+    sdl.SDL_Init.return_value = 1
+    sdl.sdlimage.IMG_Init.return_value = -1
     with pytest.raises(SystemExit):
         c = core.Core(None, None)
